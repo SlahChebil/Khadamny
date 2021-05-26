edee8cb6ed23f7f6812136820a3dcccb1f6dd437
Sleh Chebil <sleh.chebil.sc@gmail.com>
2021-05-27 01:59:10 +0200

    find jobs

--- src/FindJobs.js
+++ src/FindJobs.js
@@ -7,19 +7,128 @@ import WorkIcon from '@material-ui/icons/Work';
 function FindJobs() {
     return (
         <div className="findjobs">
-            <div className="post effect4">
-                <div className="contenu">
-                    <BusinessIcon className="img"></BusinessIcon>
-                    <h4><strong>Human Resources Administrator</strong></h4>
-                    <p>The company Name</p>
-                    <p>New York, NY</p>
-                    <ul>
-                        <li><MonetizationOnIcon></MonetizationOnIcon>600 TND </li>
-                        <li><WorkIcon></WorkIcon>IT Manager</li>
-                    </ul>
-                    <p>He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+            <div className="container">
+                <div className="recherche">
+                    <form className="formrech">
+                        <input type="text" className="inputsrech" placeholder="Job title, Keywords" id="job"/>
+                        <input type="text" className="inputsrech" placeholder="City, state, zip code" id="area"/>
+                        <button id="btnrech">FIND JOBS</button>
+                    </form>
                 </div>
-
+                <div className="post">
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                    <div className="contenu">
+                        <BusinessIcon className="img"></BusinessIcon>
+                        <h4>Human Resources Administrator</h4>
+                        <p>VERMEG</p>
+                        <p>New York, NY</p>
+                        <ul>
+                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND </li>
+                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                        </ul>
+                        <div className="foter">
+                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                            <button className="apply same">Apply</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ src/Headerlogged.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Headerlogged() {
+    return (
+        <div className="headerlogged">
+            
+        </div>
+    )
+}
+
+export default Headerlogged
