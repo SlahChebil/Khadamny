cb2c6cae034de787bce46993ea2ee87de3de2f14
Sleh Chebil <sleh.chebil.sc@gmail.com>
2021-05-30 00:03:00 +0200

    api

--- src/Createep.js
+++ src/Createep.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+export class Post extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            c_password: '',
+            phone: '',
+            city:'',
+        }
+    }
+    changeHandler =(e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('url',this.state).then(res => {console.log(res)}).catch(er => {console.log(er)})
+    }
+}
 
 function Createep() {
+    const {name, email , password , c_password ,phone ,city} = this.state;
     return (
         <div className="createep">
             <div className="container">
@@ -16,11 +39,12 @@ function Createep() {
                                 <li className="choice " id="jobseeker"><Link to="createsp"><a href="#" id="link2">I'm a Job Seeker</a></Link></li>
                             </ul>
                         </div>
-                        <input type="text" name="name" placeholder="Name"/>
-                        <input type="email" placeholder="Email Adress" id="email" required/><br/>
-                        <input type="password" placeholder="Password" id="password" required/><br/>
-                        <input type="password" name="c_password" id="c_password" placeholder="Confirm password" required/>
-                        <input type="tel" name="phone"placeholder="Phone number" pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}"/>
+                        <input type="text" name="name" placeholder="Name" value={name} onChange={this.changeHandler}/>
+                        <input type="email" placeholder="Email Adress" id="email" value={email} onChange={this.changeHandler}required/><br/>
+                        <input type="password" placeholder="Password" id="password" value={password} onChange={this.changeHandler} required/><br/>
+                        <input type="password" name="c_password" id="c_password" placeholder="Confirm password" value={c_password} onChange={this.changeHandler}required/>
+                        <input type="tel" name="phone"placeholder="Phone number" pattern="[0-9]{2}-[0-9]{3}-[0-9]{3}" value={phone} onChange={this.changeHandler}/>
+                        <input type="text" placeholder="city" id="city" value={city} onChange={this.changeHandler} required /><br/>
                         <div className="submit">
                             <button type="submit" className="ghh">SIGN UP</button>
                         </div>
