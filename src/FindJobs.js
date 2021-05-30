2dea15e70b58c7b5d0cad6d52b0e3bd296ed6653
Sleh Chebil <sleh.chebil.sc@gmail.com>
2021-05-30 03:56:13 +0200

    dfdfdf

--- src/FindJobs.js
+++ src/FindJobs.js
@@ -1,4 +1,4 @@
-import React, { Component , useState }from 'react';
+import React, { Component , useState , useEffect }from 'react';
 
 import './style.css';
 import BusinessIcon from '@material-ui/icons/Business';
@@ -7,6 +7,28 @@ import WorkIcon from '@material-ui/icons/Work';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 
 export class FindJobs extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            categorie:'',
+            city:'',
+        }
+    }
+    changeHandler =(e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    submitHandler =(e) => {
+        e.preventDefault()
+        console.log(this.state)
+        return axios.get('url')
+        .then(res => {
+            console.log(res);
+            return res;
+        })
+        .catch(er => {
+            console.log(er);
+        })
+    }
     
     render() {
         const popup = () => {
@@ -17,31 +39,35 @@ export class FindJobs extends Component{
             let a = document.querySelector(".popup");
             a.style.display= "none";
         }
+        const {city, categorie} = this.state;
+        const [posts, setpost] = useState(JSON.stringify(res))
         return(
         <div className="findjobs">
             <div className="container">
                 <div className="recherche">
-                    <form className="formrech">
-                        <input type="text" className="inputsrech" placeholder="Job title, Keywords" id="job"/>
-                        <input type="text" className="inputsrech" placeholder="City, state, zip code" id="area"/>
-                        <button id="btnrech">FIND JOBS</button>
+                    <form className="formrech" onSubmit={this.submitHandler}>
+                        <input type="text" className="inputsrech" placeholder="Job title, Keywords" id="job" name="categorie" value={categorie} onChange={this.changeHandler}/>
+                        <input type="text" className="inputsrech" placeholder="City, state, zip code" id="area" name="city" value={city} onChange={this.changeHandler}/>
+                        <button id="btnrech" type="submit">FIND JOBS</button>
                     </form>
                 </div>
-                <div className="post" onClick={()=>popup()}>
-                    <div className="contenu">
-                        <BusinessIcon className="img"></BusinessIcon>
-                        <h4>Human Resources Administrator</h4>
-                        <p>VERMEG</p>
-                        <p>New York, NY</p>
-                        <ul>
-                            <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND</li>
-                            <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
-                        </ul>
-                        <div className="foter">
-                            <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
-                            <button className="apply same" onClick={()=>popup()}>Click Apply</button>
-                        </div>
-                    </div>
+                <div className="post" onClick={()=>popup()}>   
+                        {posts.map((post)=>(
+                            <div className="contenu">
+                                <BusinessIcon className="img"></BusinessIcon>
+                                <h4>{post.title}</h4>
+                                <p>VERMEG</p>
+                                <p>New York, NY</p>
+                                <ul>
+                                    <li><MonetizationOnIcon className="icon"></MonetizationOnIcon>600 TND</li>
+                                    <li><WorkIcon className="icon"></WorkIcon>IT Manager</li>
+                                </ul>
+                                <div className="foter">
+                                    <p className="description same">He or She will work closely with the HR Manager and should have strong background experience in the Human Resources field, preferably staffing and recruitment.</p>
+                                    <button className="apply same" onClick={()=>popup()}>Click Apply</button>
+                                </div>
+                            </div>
+                        ))}        
                 </div>
                 <div className="popup" id="popup">
                         <div className="popup_content">
